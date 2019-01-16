a7e69f53291245ab820a0a79503bf7fa64595a1d
Kristjan Laht <kristjan.laht13@gmail.com>
2019-01-16 14:10:48 +0200

    new output

--- anonimize.py
+++ anonimize.py
@@ -13,10 +13,16 @@ def anonymize(filename, name_column):
     df = pd.read_csv(filename)
 
     df[name_column] = df[name_column].apply(lambda x: d[x])
+
     return df
 
 
-df = pd.read_csv("common.csv")
+df = pd.read_csv("common2.csv")
 
 c1 = "Your name (or a made up name if you prefer so - just make sure to use the same name every time!)"
 c2 = "Your name (or a made up name if you prefer so - just make sure to use the same name every time and it's the same name you gave in the general questionnaire!)"
+
+common = anonymize("common2.csv", c2)
+common.to_csv("out_common.csv")
+general = anonymize("general.csv", c1)
+general.to_csv("out_general.csv")
